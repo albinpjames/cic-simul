f39f5c45601e551d24f2fb2670a51b23ec095b1f
albinpjames <mail@albinpjames.me>
2024-05-17 01:08:11 +0530

    v1

--- sparkhalo.py
+++ sparkhalo.py
@@ -1,3 +1,9 @@
+
+import os
+from pathlib import Path
+import numpy as np
+import math
+from abacusnbody.data.compaso_halo_catalog import CompaSOHaloCatalog
 from scipy.stats import binned_statistic_dd 
 
 class SimulData(object):
@@ -9,7 +15,8 @@ class SimulData(object):
         self.boxsize = boxsize if boxsize is not None else params["boxsize"]  
         self.mass = mass if mass is not None else params["mass"]
 
-    def cic(self,nw_boxsize):
+    def cic(self,data,nw_boxsize):
+        
         cutside = int(self.boxsize/nw_boxsize)
 
         x_bins_dd = pos_start[0] + np.linspace(0,nw_boxsize*cutside,cutside+1)
@@ -44,8 +51,41 @@ class abacussummit(SimulData):
         self.dataloc = dataloc if dataloc is not None else params["dataloc"]
     
     def readdata(self, redshift: float = None, clms: list = None):
+        print("Reading the data")
 
-    
+        # Location of the data
+        file = os.path.join(
+            params.datadirec,
+            "Simulations/AbacusSummit_Public_Data_Access/AbacusSummit_"
+            + params.type
+            + "_"
+            + params.cosmo
+            + "_"
+            + params.intcont,
+            "halos/z" + redshift,
+            "halo_info/",
+        )
+
+        self.cat = CompaSOHaloCatalog(file, cleaned=False)  # Reads the data
+        self.cat = self.cat.halos[clms]  # Reads the given column and saves it to an array
+
+        if "N" in clms:
+            print("converting to mass")
+            self.cat["halomass"] = self.cat["N"] * self.mass
+        
+        if "SO_central_particle" in clms:
+            self.cat["SO_central_particle"] += self.boxsize / 2
+
+            self.cat.add_columns(
+                [
+                    self.cat["SO_central_particle"][:, 0],
+                    self.cat["SO_central_particle"][:, 1],
+                    self.cat["SO_central_particle"][:, 2],
+                ],
+                names=["xpos", "ypos", "zpos"],
+            )
+
+            del self.cat["SO_central_particle"]
 
     
 
@@ -61,15 +101,37 @@ if __init__ == '__main__':
         "mass": 2.109081520453063 * 10**9,  # The mass of the particles in the simulation
         }
     
-    simulation = abacussummit(params)
+    simul = abacussummit(params)
+
     """ Redshifts & boxsizes to be computed """
-    # redshifts = ["3.000","2.500","2.000"]
     redshifts = ["3.000"]
-    # nw_boxsizes = [30]
     nw_boxsizes = [50,15,10,5]
+    halobins = 20
    
     for redshift in redshifts:
         print(f"redshift being computed: {redshift}")
         clms = ["N","SO_central_particle"]
-        simulation.readdata(redshift,clms)
+        simul.readdata(redshift,clms)
+
+        for nw_boxsize in nw_boxsizes: 
+
+            print(f"Boxsize being computed: {nw_boxsize}")
+            cutside = int(simul.boxsize/nw_boxsize)
+            print(f"The times the box length is cut: {cutside}")
+            totalboxes = int(cutside**3) 
+            print(f"The total number of boxes: {totalboxes}")
+
+            cicdata = np.zeros((totalboxes,halobins))
+
+
+            binedge = np.logspace(
+                np.log(np.min(simul.cat["halomass"])),
+                np.log(np.max(simul.cat["halomass"])),
+                halobins + 1,
+                base=math.e)
+
+            for i in range(halobins):
+                data = simul.cat[["xpos","ypos","zpos"]][(binedge[i] <= simul.cat["halomass"]) & (simul.cat["halomas"] < binedge[i + 1])]
+                data = np.array([data['xpos'], data['ypos'], data['zpos']]).T
+                cicdata[:,i] = simul.cic(data,nw_boxsize)
 
